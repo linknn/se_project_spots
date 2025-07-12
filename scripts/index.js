e08a7e9e7b91ed71c679100117715c4206a2700d
linknn <crysnnelson@hotmail.com>
2025-07-12 16:19:47 -0700

    Add click events to open and remove edit and new post modals

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,23 @@
 const profileEditBtn = document.querySelector(".profile__edit-btn");
-const editProfileModal = document.querySelector("#edit-profile-modal");
-const profileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const profileEditModal = document.querySelector("#edit-profile-modal");
+const profileEditCloseBtn = profileEditModal.querySelector(".modal__close-btn");
+
+const newPostBtn = document.querySelector(".profile__new-post");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+
+profileEditBtn.addEventListener("click", function () {
+  profileEditModal.classList.add("modal_is-opened");
+});
+
+profileEditCloseBtn.addEventListener("click", function () {
+  profileEditModal.classList.remove("modal_is-opened");
+});
+
+newPostBtn.addEventListener("click", function () {
+  newPostModal.classList.add("modal_is-opened");
+});
+
+newPostCloseBtn.addEventListener("click", function () {
+  newPostModal.classList.remove("modal_is-opened");
+});
