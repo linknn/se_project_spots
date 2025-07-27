23ce7a5a226f6e0d258d3da43b2f98f8e5cee0a3
linknn <crysnnelson@hotmail.com>
2025-07-27 01:14:08 -0700

    Select and append cards to card list

--- scripts/index.js
+++ scripts/index.js
@@ -5,13 +5,13 @@ const initialCards = [
   },
 
   {
-    name: "Waving owl",
-    link: "https://images.unsplash.com/photo-1516233758813-a38d024919c5?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Owl eye close up",
+    link: "https://plus.unsplash.com/premium_photo-1675188410515-f5886e29c09e?q=80&w=684&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 
   {
-    name: "White and brown owl on a petaled field",
-    link: "https://images.unsplash.com/photo-1467811884194-ae868cd3f090?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Waving owl",
+    link: "https://images.unsplash.com/photo-1516233758813-a38d024919c5?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 
   {
@@ -20,8 +20,8 @@ const initialCards = [
   },
 
   {
-    name: "Owl eye close up",
-    link: "https://plus.unsplash.com/premium_photo-1675188410515-f5886e29c09e?q=80&w=684&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "White and brown owl on a petaled field",
+    link: "https://images.unsplash.com/photo-1467811884194-ae868cd3f090?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 
   {
@@ -54,6 +54,7 @@ const newPostLinkInput = document.querySelector("#card-image-input");
 const cardTemplate = document
   .querySelector("#card-template")
   .content.querySelector(".card");
+const cardsList = document.querySelector(".cards__list");
 
 function getCardElement(data) {
   const cardElement = cardTemplate.cloneNode(true);
@@ -114,5 +115,6 @@ function handleAddCardSubmit(evt) {
 newPostFormEl.addEventListener("submit", handleAddCardSubmit);
 
 initialCards.forEach(function (item) {
-  console.log(getCardElement(item));
+  const card = getCardElement(item);
+  cardsList.append(card);
 });
