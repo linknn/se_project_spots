a72356d81707f028462604291a82273993ebc54c
linknn <crysnnelson@hotmail.com>
2025-08-03 00:26:20 -0700

    Create and link validation script for modal inputs; add error ids to modal span errors

---
+++ scripts/validation.js
@@ -0,0 +1,32 @@
+const showInputError = (formEl, inputEl, errorMsg) => {};
+
+const checkInputValidity = (formEl, inputEl) => {
+  console.log(inputEl.validationMessage);
+  if (!inputEl.validity.valid) {
+    showInputError(formEl, inputEl, inputEl.validationMessage);
+  }
+};
+
+const setEventListeners = (formEl) => {
+  const inputList = Array.from(formEl.querySelectorAll(".modal__input"));
+  const buttonElement = formEl.querySelector(".modal__submit");
+
+  //  TODO - handle initial states
+  //   toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputEl) => {
+    inputEl.addEventListener("input", function () {
+      checkInputValidity(formEl, inputEl);
+      // toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll(".modal__form"));
+  formList.forEach((formEl) => {
+    setEventListeners(formEl);
+  });
+};
+
+enableValidation();
