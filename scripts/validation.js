e1ccdb8010bf7569868d7a8cddcfe86d067ea360
linknn <crysnnelson@hotmail.com>
2025-08-03 01:31:51 -0700

    Create function to hide error message and add styles ot input when error is active

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,12 +1,21 @@
 const showInputError = (formEl, inputEl, errorMsg) => {
-  const errorMsgEl = document.querySelector(`#${inputEl.id}-error`);
+  const errorMsgEl = formEl.querySelector(`#${inputEl.id}-error`);
   errorMsgEl.textContent = errorMsg;
+  inputEl.classList.add("modal__input_type_error");
+};
+
+const hideInputError = (formEl, inputEl, errorMsg) => {
+  const errorMsgEl = formEl.querySelector(`#${inputEl.id}-error`);
+  errorMsgEl.textContent = "";
+  inputEl.classList.remove("modal__input_type_error");
 };
 
 const checkInputValidity = (formEl, inputEl) => {
   console.log(inputEl.validationMessage);
   if (!inputEl.validity.valid) {
     showInputError(formEl, inputEl, inputEl.validationMessage);
+  } else {
+    hideInputError(formEl, inputEl);
   }
 };
 
